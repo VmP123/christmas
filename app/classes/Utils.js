ec4d4025760842eff0285ab375de7b4d17e0e253
VmP123 <vpasanen@gmail.com>
2018-11-30 20:53:31 +0200

    Trailing spaces removed

--- app/classes/Utils.js
+++ app/classes/Utils.js
@@ -1,8 +1,8 @@
 export default class Utils {
 	static testCollision(a, b) {
-		return (a.x < b.x + b.width && 
+		return (a.x < b.x + b.width &&
 			a.x + a.width > b.x &&
-			a.y < b.y + b.height && 
+			a.y < b.y + b.height &&
 			a.y + a.height > b.y);
 	}
 }
